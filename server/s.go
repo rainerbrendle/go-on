513970e8cb383225fa3d54173f9e81d5dc3c96be
rainerbrendle <rainer.brendle@gmail.com>
2023-03-30 19:55:25 -0400

    Database Server

--- server/s.go
+++ server/s.go
@@ -3,9 +3,11 @@ package server
 import (
 	"go-on.com/server/controller"
 	"go-on.com/server/model"
+	"go-on.com/server/data"
 )
 
 func Run() {
+   data.DbConnect()
    model.Init()
    controller.Start()
 }
